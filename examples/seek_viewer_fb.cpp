0b7bcffe51c5cfca29b79c5c5ed6fca21ad85a1c
Christopher Rooney <christopher@rooneyworks.com>
2020-09-11 03:21:12 +0100

    unknown changes

--- examples/seek_viewer_fb.cpp
+++ examples/seek_viewer_fb.cpp
@@ -10,19 +10,21 @@
 #include <signal.h>
 #include <memory>
 #include "args.h"
-//for handling framebuffer
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <linux/fb.h>
-#include <sys/mman.h>
-#include <sys/ioctl.h>
-#include <wiringPi.h>
 #include <fstream>
+#include "framebuffer.h"
+#include <wiringPi.h>
 
 using namespace cv;
 using namespace LibSeek;
+
+struct ButtonState
+{
+    int b17=0;
+    int b22=0;
+    int b23=0;
+    int b27=0;
+    int battery=0;
+};
 
 // Setup sig handling
 static volatile sig_atomic_t sigflag = 0;
@@ -31,26 +33,7 @@ void handle_sig(int sig)
     (void)sig;
     sigflag = 1;
 }
-// cv::Mat openFile(std::string fname, cv::Mat &m_raw_frame)
-// {
-//     cv::Mat m_additional_ffc;
-//     if (fname != std::string()) {
-//         m_additional_ffc = cv::imread(fname, -1);
 
-//         if (m_additional_ffc.type() != m_raw_frame.type()) {
-//             std::cerr<<"Error: "<<fname.c_str()<<"not found or it has the wrong type: "<<m_additional_ffc.type()<<std::endl;
-//             return cv::Mat::zeros(0,0,1);
-//         }
-
-//         if (m_additional_ffc.size() != m_raw_frame.size()) {
-//             std::cerr<<"Error: expected "<<fname.c_str()<<" to have size ["<<m_raw_frame.cols<<","<<m_raw_frame.rows<<"], got ["<<m_additional_ffc.cols<<","<<m_additional_ffc.rows<<"]"<<std::endl;
-//             return cv::Mat::zeros(0,0,1);
-//         }
-//     }
-
-//     return m_additional_ffc;
-// }
-// Function to process a raw (corrected) seek frame
 void process_frame(Mat &inframe, Mat &outframe, float scale, int colormap, int rotate, int last[], int staticMin, int staticMax)
 {
     
@@ -64,14 +47,12 @@ void process_frame(Mat &inframe, Mat &outframe, float scale, int colormap, int r
 
     double raw_maxvalue;
     cv::minMaxIdx(inframe,0,&raw_maxvalue);
-    //std::cout << raw_maxvalue << std::endl;
 
     low=imMean[0]-2*imStd[0];
     high=imMean[0]+4*imStd[0];
 
     //slower min/max handling. In the old code, bad pixels would cause the colorbar scale
     //to jump all over the place. The averaging I do here slows that down so that colorbar scale varys slower.
-    //Still this is not the best solution because a big thing of constant temperature looks hotter near edges of screen.
     last[0]=(2*last[0]+low)/3;
     last[1]=(2*last[1]+high)/3;
 
@@ -85,7 +66,6 @@ void process_frame(Mat &inframe, Mat &outframe, float scale, int colormap, int r
     else
         max=last[1];
     
-    //std::cout<<min<<" "<<max<<std::endl;
     frame_g16=(inframe-min)*(65535.0/(max-min));
     //normalize(inframe, frame_g16, 0, 65535, NORM_MINMAX,-1,mask);
     // Convert seek CV_16UC1 to CV_8UC1
@@ -147,8 +127,7 @@ void new_flat(LibSeek::SeekCam* seek)
     std::cout<<"Flat field finished"<<std::flush;
 }
 
-void save_image(cv::Mat &seekframe, cv::Mat &outframe)
-{
+std::string get_next_file_num_and_increment(){
     std::ifstream currentnumfile;
     std::ofstream nextnumfile;
     currentnumfile.open("./imgs/.nextnum");
@@ -161,14 +140,51 @@ void save_image(cv::Mat &seekframe, cv::Mat &outframe)
         outnum="0"+outnum;
     }
 
-    imwrite("./imgs/img_"+outnum+"_raw.png",seekframe);
-    imwrite("./imgs/img_"+outnum+".png",outframe);
-
     nextnumfile.open("./imgs/.nextnum");
     nextnumfile << currentnum+1;
     nextnumfile.close();
     std::cout <<std::endl<< "saved image ./imgs/img_"+outnum+".png"<<std::flush;
+    return outnum;
 }
+
+void save_image(cv::Mat &seekframe, cv::Mat &outframe)
+{
+    std::string outnum = get_next_file_num_and_increment();
+    imwrite("./imgs/img_"+outnum+"_raw.png",seekframe);
+    imwrite("./imgs/img_"+outnum+".png",outframe);
+}
+
+void setupButtons(){
+    //setup gpio buttons
+    wiringPiSetup();
+    //4 buttons on screen
+    pinMode(0,INPUT);
+    pullUpDnControl(0,PUD_UP);
+    pinMode(3,INPUT);
+    pullUpDnControl(3,PUD_UP);
+    pinMode(4,INPUT);
+    pullUpDnControl(4,PUD_UP);
+    pinMode(2,INPUT);
+    pullUpDnControl(2,PUD_UP);
+    //battery low
+    pinMode(7,INPUT);
+}
+
+void updatepin(int pinNum, int& priorState){
+    if (!digitalRead(pinNum))
+        priorState++;
+    else
+        priorState=0;
+}
+
+void readButtons(ButtonState& priorState){
+        updatepin(0,priorState.b17);
+        updatepin(3,priorState.b22);
+        updatepin(4,priorState.b23);
+        updatepin(2,priorState.b27);
+        updatepin(7,priorState.battery);
+}
+
 
 int main(int argc, char** argv)
 {
@@ -179,13 +195,9 @@ int main(int argc, char** argv)
     args::ValueFlag<std::string> _output(parser, "output", "Output Stream - name of the video file to write", {'o', "output"});
     args::ValueFlag<std::string> _ffc(parser, "FFC", "Additional Flat Field calibration - provide ffc file", {'F', "FFC"});
     args::ValueFlag<int> _fps(parser, "fps", "Video Output FPS - Kludge factor", {'f', "fps"});
-    args::ValueFlag<float> _scale(parser, "scaling", "Output Scaling - multiple of original image", {'s', "scale"});
     args::ValueFlag<int> _colormap(parser, "colormap", "Color Map - number between 0 and 12", {'c', "colormap"});
     args::ValueFlag<int> _rotate(parser, "rotate", "Rotation - 0, 90, 180 or 270 (default) degrees", {'r', "rotate"});
     args::ValueFlag<std::string> _camtype(parser, "camtype", "Seek Thermal Camera Model - seek or seekpro", {'t', "camtype"});
-    args::ValueFlag<int> _scalemin(parser, "scalemin", "Minimum value of color scale", {'m', "scalemin"});
-    args::ValueFlag<int> _scalemax(parser, "scalemax", "Maximum value of color scale", {'M', "scalemax"});
-    args::Flag _doLogScale(parser, "logscale", "Whether to use a log scale", {'l',"logscale"});
     // Parse arguments
     try
     {
@@ -212,8 +224,6 @@ int main(int argc, char** argv)
     int staticMin = -1;
     int staticMax = -1;
 
-    if (_scale)
-        scale = args::get(_scale);
     std::string output = "window";
     if (_output)
         output = args::get(_output);
@@ -234,74 +244,15 @@ int main(int argc, char** argv)
     if (_rotate)
         rotate = args::get(_rotate);
 
-    if (_scalemin)
-        staticMin = args::get(_scalemin);
-
-    if (_scalemax)
-        staticMax = args::get(_scalemax);
-    bool logScale=false;
-    if (_doLogScale)
-        logScale = args::get(_doLogScale);
-
-
     // Register signals
     signal(SIGINT, handle_sig);
     signal(SIGTERM, handle_sig);
 
     //setup framebuffer
-    int fbfd = 0;
-    struct fb_var_screeninfo vinfo;
-    struct fb_fix_screeninfo finfo;
-    long int screensize = 0;
-    char *fbp = 0;
+    Framebuffer fbwriter("/dev/fb0");
 
-    // Open the file for reading and writing
-    fbfd = open("/dev/fb0", O_RDWR);
-    if (fbfd == -1) {
-        perror("Error: cannot open framebuffer device");
-        exit(1);
-    }
-    printf("The framebuffer device was opened successfully.\n");
-    // Get fixed screen information
-    if (ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo) == -1) {
-        perror("Error reading fixed information");
-        exit(2);
-    }
-    // Get variable screen information
-    if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo) == -1) {
-        perror("Error reading variable information");
-        exit(3);
-    }
-    printf("%dx%d, %dbpp\n", vinfo.xres, vinfo.yres, vinfo.bits_per_pixel);
-    // Figure out the size of the screen in bytes
-    screensize = finfo.smem_len;//vinfo.xres * vinfo.yres * vinfo.bits_per_pixel / 8;
-    // Map the device to memory
-    std::cout << finfo.line_length << " " << vinfo.xres*vinfo.bits_per_pixel/8 << " " <<std::endl;
-                    // location = (vinfo.xres+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
-                    //    (vinfo.yres+vinfo.yoffset) * finfo.line_length;
-    fbp = (char *)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED,
-                       fbfd, 0);
-    if ((int64_t)fbp == -1) {
-        perror("Error: failed to map framebuffer device to memory");
-        exit(4);
-    }
-    printf("The framebuffer device was mapped to memory successfully.\n");
-
-
-    //setup gpio buttons
-    wiringPiSetup();
-    //4 buttons on screen
-    pinMode(0,INPUT);
-    pullUpDnControl(0,PUD_UP);
-    pinMode(3,INPUT);
-    pullUpDnControl(3,PUD_UP);
-    pinMode(4,INPUT);
-    pullUpDnControl(4,PUD_UP);
-    pinMode(2,INPUT);
-    pullUpDnControl(2,PUD_UP);
-    //battery low
-    pinMode(7,INPUT);
-
+    setupButtons();
+    ButtonState bs;
     // Setup seek camera
     LibSeek::SeekCam* seek;
     LibSeek::SeekThermalPro seekpro(args::get(_ffc));
@@ -328,14 +279,7 @@ int main(int argc, char** argv)
     int last [2];
     last[0]=0;
     last[1]=65535;
-    float xscale,yscale;
-    xscale = static_cast<float>(vinfo.xres)/seekframe.cols;
-    yscale = static_cast<float>(vinfo.yres)/seekframe.rows;
-    if (xscale < yscale){
-        scale = xscale;
-    }else{
-        scale = yscale;
-    }
+    scale=fbwriter.getScale(seekframe);
     //std::cout << xscale << yscale << scale << std::endl;
     process_frame(seekframe, outframe, scale, colormap, rotate,last,staticMin,staticMax);
     //std::vector<cv::Point> dp = {cv::Point(212,34),cv::Point(212,35)};
@@ -352,8 +296,7 @@ int main(int argc, char** argv)
 
         std::cout << "Video stream created, dimension: " << outframe.cols << "x" << outframe.rows << ", fps:" << fps << std::endl;
     }
-    int row, col, location, shutdowncounter, textcounter,rowsToCopy;
-    shutdowncounter=0;
+    int textcounter;
     textcounter=0;
     // Main loop to retrieve frames from camera and output
     while (!sigflag) {
@@ -364,108 +307,93 @@ int main(int argc, char** argv)
         }
         // Retrieve frame from seek and process
 
-        if(logScale){ 
-            seekframe.convertTo(seekframe,CV_64F);
-            cv::log(seekframe,seekframe);
-        }
-
         process_frame(seekframe, outframe, scale, colormap, rotate,last,staticMin,staticMax);
         if (output != "window") {
             writer << outframe;
         }
-        //write image to framebuffer
-        //bool flag = true;
-        if (textcounter>0){
-            rowsToCopy = outframe.rows-12;
-            textcounter --;
-        } else
-            rowsToCopy = outframe.rows;
-        for (row = 0; row < rowsToCopy; row++) {
-            uint8_t* rowptr = outframe.ptr(row);
-            //flag = true;
-            for (col=0; col<outframe.cols; col++) {
-                location = (col+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
-                       (row+vinfo.yoffset) * finfo.line_length;
-                if(vinfo.bits_per_pixel==32){
-                    // if (row > 760){
-                    // std::cout << screensize<< " "<<location << std::endl;
-                    // }   
-                    *(fbp + location) = rowptr[3*col];
-                    *(fbp + location+1) = rowptr[3*col+1];
-                    *(fbp + location+2) = rowptr[3*col+2];
-                    //note neither of these are RGB, they're both BGR
-                    *(fbp + location+3) = 0;
-                    // if(flag){
-                    //     printf("%d %d %d\n",red,green,blue);
-                    //     flag = false;
-                    // }
-                }else if(vinfo.bits_per_pixel==16){
-                    //red 5bit, green 6bit, blue 5bit
-                    uint8_t blue=rowptr[3*col]>>3;
-                    uint8_t green=rowptr[3*col+1]>>2;
-                    uint8_t red=rowptr[3*col+2]>>3;
-                    uint8_t byte1=(red<<3)+(green>>3);
-                    uint8_t byte2=((green%8)<<5)+blue;
-                    *(fbp+location)=byte2;
-                    *(fbp+location+1)=byte1;
-                    //if(flag){
-                    //    printf("%d %d %d\n",red,green,blue);
-                    //    flag = false;
-                    //}
 
-                }else{
-                    //panic
-                    printf("bits per pixel == %d NYI",vinfo.bits_per_pixel);
-                    return (1);
-                }
-            }
-        }
-        bool pin17=!digitalRead(0);
-        bool pin22=!digitalRead(3);
-        bool pin23=!digitalRead(4);
-        bool pin27=!digitalRead(2);
-        bool pin4=digitalRead(7);
-        if (!pin4){
-            std::cout<<std::endl<<"pin 4 hi"<<std::flush;
-            textcounter=20;
-        }
-        if (pin17){
+        readButtons(bs);
+        //bs numbers increase as long as they're held
+        //down. So if you want someting to trigger exactly
+        //once, use bs.b##==1
+
+        //Check for low battery state
+
+
+        //Check if user wants a flat frame
+        if (bs.b27>20){
             //do flat field
             new_flat(seek);
         }
-        if (pin22){
+
+        //Check if user wants to toggle colorbar mode
+        if (bs.b23==1 && staticMin >=0 && staticMax >=0){
             //change colorbar mode to variable
             staticMin = -1;
             staticMax = -1;
             std::cout<<std::endl<<"set cbar to variable"<<std::flush;
-            textcounter=8;
-        }
-        if (pin23){
+            textcounter=20;
+        } else if (bs.b23==1 && staticMin ==-1 && staticMax ==-1){
             staticMin = last[0];
             staticMax = last[1];
-            std::cout<<std::endl<<"reset colorbar"<<std::flush;
-            textcounter=8;
-        }
-        if (pin27){
+            std::cout<<std::endl<<"set static colorbar"<<std::flush;
+            textcounter=20;
+        } 
 
-            if (shutdowncounter>20) {
-                system("sudo shutdown now");
-            } 
-            else if (shutdowncounter == 0){
-                save_image(seekframe,outframe);
-            } else if (shutdowncounter == 5){
-                std::cout<<std::endl<<"Hold to shutdown"<<std::flush;
+        //Check if user wants to initiate shutdown.
+        //Give hint in case I forget the new shutdown logic
+        if (bs.b23 > 20 && bs.b17 > 20){
+            system("sudo shutdown now");
+        } else if (bs.b23 >20){
+            std::cout << std::endl<<"Hold buttons 27 and 17 to shutdown"<<std::flush;
+            textcounter=20;
+        }
+        //save snapshot
+        if (bs.b17 == 1){
+            save_image(seekframe,outframe);
+            textcounter=20;
+        } 
+
+        //start videotaping?
+        if (bs.b22 ==10 && output=="window"){
+            output = "imgs/img_"+ get_next_file_num_and_increment()+"_vid.mp4";
+            writer.open(output, VideoWriter::fourcc('m', 'p', '4', 'v'), fps, Size(outframe.cols, outframe.rows));
+            if (!writer.isOpened()) {
+                std::cerr << "Error can't create video writer" << std::endl;
+                return 1;
             }
-            textcounter=10;
-            shutdowncounter++;
+        } else if (bs.b22 ==1 && output !="window"){
+            writer.release();
+            output="window";
+        }
+
+        //render high priority text
+        if (output != "window"){
+            std::cout<<std::endl<< "RECORDING"<<std::flush;
+            textcounter=1;
+        }
+        //render higher priority text
+        if (bs.battery%20==1){
+            std::cout<<std::endl<<"LOW BATTERY"<<std::flush;
+            textcounter=20;
+        } else if(bs.battery > 500){
+            system("sudo shutdown now");
+        }
+        if (textcounter>0) {
+            textcounter --;
+            fbwriter.setUncopiedRows(12);
         } else {
-            shutdowncounter=0;
+            fbwriter.setUncopiedRows(0);
+        }
+        //render image after everything to cover up extraneous text
+        //keep the image onscreen as long as they hold button 17
+        if (bs.b17<=1){
+            fbwriter.writeFrame(outframe);
         }
 
 
     }
-    munmap(fbp,screensize);
-    close(fbfd);
+    fbwriter.closefb();
     //cv::Mat outfile;
     //outframe.convertTo(outfile, CV_16UC3);
     cv::imwrite("lastimg_raw.png", seekframe);
